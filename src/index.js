3906e1eb9cc35d00672dd03b4965bce4f356da58
Ederson Ferreira <edersondeveloper@gmail.com>
2020-06-24 18:08:37 -0400

    Fixing CSS properties and bug if a themes dir does not exit

--- src/index.js
+++ src/index.js
@@ -17,13 +17,13 @@ fs.readFile(files[0], 'utf8', (err, data) => {
 
 function pagePDF(html) {
   (async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setContent(html);
 
-    files[1] == undefined ? (files[1] = 'themes/default.css') : null;
+	const defaultCSS = "body{font-family:Arial, Helvetica, sans-serif;margin-top:-1010px;}h1{margin-top:1010px;text-align:center;}p{text-align:justify;}table{border-collapse:collapse;margin-left:auto;margin-right:auto;}table,th,td{border:1px solid black;padding:10px;}pre{background-color:#282a36;color:#f8f8f2;display:block;border-radius:5px;padding:5px;}code{background-color:#282a36;color:#f8f8f2;border-radius:5px;}a{text-decoration:none;}"
 
-    await page.addStyleTag({ path: files[1] });
+    files[1] == undefined ? await page.addStyleTag({ content: defaultCSS }) : await page.addStyleTag({ path: files[1] });
     await page.emulateMedia('screen');
 
     pdfFile = files[0].replace('.md', '.pdf');
